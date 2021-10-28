26cb5de8c46c0dce7a79814e0501bda802beebb3
Rarib <59053047+MattCornicDev@users.noreply.github.com>
2021-10-28 10:36:58 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  cercle: Cercle;
+  perimetre: number;
+  surface: number;
   constructor() {}
+  CalculsCercle(r: number)
+  {
+    this.cercle = new Cercle(r);
+    this.perimetre = this.cercle.perimetre();
+    this.surface = this.cercle.surface();
+  }
 
+}
+export class Cercle{
+  private rayon: number;
+
+  constructor(r: number)
+  {
+    this.rayon=r;
+  }
+
+  public perimetre(): number
+  {
+    return (Math.round((2* 3.1415 * this.rayon)*100)/100);
+  }
+
+  public surface(): number
+  {
+    return ((3.14 * (Math.round(Math.pow((this.rayon * 2),2))/4)*100)/100);
+  }
 }
